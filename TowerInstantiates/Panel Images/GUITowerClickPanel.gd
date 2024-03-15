b3877a973e713a0f0a1d6089ad47bef647387b9e
KhoaNguyen <117057796+KoaHaz@users.noreply.github.com>
2024-03-15 00:59:41 -0500

    fixed minor error
    This reverts commit d0458663a3d7852e265de648e2c3e812d5928c47, reversing
    changes made to 898e70704233f52cbadeffb3dc00e4e74956bed2.

--- TowerInstantiates/Panel Images/GUITowerClickPanel.gd
+++ TowerInstantiates/Panel Images/GUITowerClickPanel.gd
@@ -3,6 +3,8 @@ extends Panel
 
 @onready var tower = preload("res://TowerInstantiates/TowerModel.tscn")
 var currTile
+var tempTower = null
+var dragging = false
 
 func _on_gui_input(event):
 	#If gold is above 100 then you can drag and drop towers
@@ -22,21 +24,10 @@ func _on_gui_input(event):
 		#Left Click Up
 		elif event is InputEventMouseButton and event.button_mask == 0:
 			print("LeftBtnUp")
-			if tempTower:
-				var path = get_tree().get_root().get_node("mainGame/Towers")
-				
-				# Manually adjust for the offset when setting the tower's position
-				var adjusted_position = get_global_mouse_position() - Vector2(0, 0)
-				var local_position = path.to_local(adjusted_position)
-				
-				tempTower.get_parent().remove_child(tempTower)
-				path.add_child(tempTower)
-				tempTower.global_position = local_position
-				tempTower.process_mode = Node.PROCESS_MODE_INHERIT
-				tempTower.get_node("Area").hide()
-				
-				GameCore.Gold -= 100
-				
-				# Reset dragging state and temporary tower reference
-				dragging = false
-				tempTower = null
+			get_child(1).queue_free()
+			var path = get_tree().get_root().get_node("mainGame/Towers")
+			
+			path.add_child(tempTower)
+			tempTower.global_position = event.global_position
+			tempTower.get_node("Area").hide()
+			GameCore.Gold -= 100
