0ddc8cffc384c4dd113e169a52b0e8bd228c8b58
Sam Parker <39849882+sam7parker@users.noreply.github.com>
2024-03-15 00:08:20 -0500

    Fixed Issue with Tower Placement

--- TowerInstantiates/Panel Images/GUITowerClickPanel.gd
+++ TowerInstantiates/Panel Images/GUITowerClickPanel.gd
@@ -1,31 +1,46 @@
 extends Panel
 
-
 @onready var tower = preload("res://TowerInstantiates/TowerModel.tscn")
-var currTile
+var tempTower = null
+var dragging = false
 
 func _on_gui_input(event):
-	#If gold is above 100 then you can drag and drop towers
+	# Check if gold is above 100 then allow drag and drop towers
 	if GameCore.Gold >= 100:
-	#------------------------------------------------------
-		var tempTower = tower.instantiate()
-		#Left Click Down
-		if event is InputEventMouseButton and event.button_mask == 1:
+		# Mouse Button Down - Begin Drag
+		if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			print("LeftBtnDown")
+			# Instantiate a tower only if we start dragging (i.e., left click down)
+			tempTower = tower.instantiate()
 			add_child(tempTower)
 			tempTower.process_mode = Node.PROCESS_MODE_DISABLED
-			
-		#Left click Drag
-		elif event is InputEventMouseButton and event.button_mask == 1:
-			get_child(1).global_position = event.global_position
-			
-		#Left Click Up
-		elif event is InputEventMouseButton and event.button_mask == 0:
+			tempTower.global_position = get_global_mouse_position()
+			dragging = true
+		
+		# Mouse Movement - Dragging
+		elif event is InputEventMouseMotion and dragging:
+			# Update the position of the tower to follow the mouse
+			if tempTower:
+				tempTower.global_position = get_global_mouse_position()
+
+		# Mouse Button Up - End Drag
+		elif event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
 			print("LeftBtnUp")
-			get_child(1).queue_free()
-			var path = get_tree().get_root().get_node("mainGame/Towers")
-			
-			path.add_child(tempTower)
-			tempTower.global_position = event.global_position
-			tempTower.get_node("Area").hide()
-			GameCore.Gold -= 100
+			if tempTower:
+				var path = get_tree().get_root().get_node("mainGame/Towers")
+				
+				# Manually adjust for the offset when setting the tower's position
+				var adjusted_position = get_global_mouse_position() - Vector2(450, 0)  # Adjusting 450px to the left
+				var local_position = path.to_local(adjusted_position)
+				
+				tempTower.get_parent().remove_child(tempTower)
+				path.add_child(tempTower)
+				tempTower.global_position = local_position
+				tempTower.process_mode = Node.PROCESS_MODE_INHERIT
+				tempTower.get_node("Area").hide()
+				
+				GameCore.Gold -= 100
+				
+				# Reset dragging state and temporary tower reference
+				dragging = false
+				tempTower = null
