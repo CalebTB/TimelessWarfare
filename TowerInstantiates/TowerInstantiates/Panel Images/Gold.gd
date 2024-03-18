2163359d7bef54e8b05fcd9e5ea99fff541eadfd
Sam Parker <39849882+sam7parker@users.noreply.github.com>
2024-03-18 04:32:52 -0500

    Delete TowerInstantiates/TowerInstantiates directory

--- TowerInstantiates/TowerInstantiates/Panel Images/Gold.gd
+++
@@ -1,5 +0,0 @@
-extends Label
-
-
-func _process(delta):
-	self.text = "Gold: " + str(GameCore.Gold)
--- TowerInstantiates/TowerInstantiates/Panel Images/Health.gd
+++
@@ -1,5 +0,0 @@
-extends Label
-
-
-func _process(delta):
-	self.text = "Health: " + str(GameCore.Health)
